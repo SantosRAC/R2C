14c13d890726bd0ec9e010568794bd393bbea61e
Renato Santos <renatoacsantos@gmail.com>
2025-07-02 12:02:25 -0300

    Added min correlation and k (topk) as paramters

--- bin/generate_correlations_corals.py
+++ bin/generate_correlations_corals.py
@@ -15,18 +15,27 @@ parser.add_argument('--expression_matrix', type=str, dest='expression_file',
 parser.add_argument('--output_network', type=str, dest='output_network',
                     help='Network file', metavar="network.txt",
                     required=True)
+parser.add_argument('--min_abs_cor', type=float, dest='min_cor',
+                    help='Minimum absolute correlation', metavar="0.5",
+                    default=0.5)
+parser.add_argument('--corals_topk_k', type=float, dest='topk_k',
+                    help='K parameter for corALS (topk)', metavar="0.05",
+                    default=0.05)
 args = parser.parse_args()
 
 matrix_file = args.expression_file
 output_network = args.output_network
+min_cor = args.min_cor
+topk_k = args.topk_k
 
 expression_matrix = pd.read_csv(matrix_file, sep="\t", index_col=0)
 expression_matrix_transposed = expression_matrix.transpose()
 expression_matrix_transposed_filtered= expression_matrix_transposed.loc[:, expression_matrix_transposed.sum(axis=0) != 0]
 
 gene_names=expression_matrix_transposed_filtered.columns.to_list()
-cor_topk_result = cor_topk(expression_matrix_transposed_filtered, k=0.05, correlation_type="pearson", n_jobs=3)
+cor_topk_result = cor_topk(expression_matrix_transposed_filtered, k=topk_k, correlation_type="pearson", n_jobs=3)
 
 with open(args.output_network, 'w') as out:
     for i, j, v in zip(cor_topk_result[1][0], cor_topk_result[1][1], cor_topk_result[0]):
-        out.write(f"{gene_names[i]}\t{gene_names[j]}\t{v}\n")
+        if abs(v) >= min_cor:
+            out.write(f"{gene_names[i]}\t{gene_names[j]}\t{v}\n")
